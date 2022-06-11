a2901c5ea17e2cb8c802e8b10f0e321571ff7ad3
david max <davidmax.dmao@gmail.com>
2022-06-11 01:09:19 -0300

    refactor

--- main/coleta_acoes.py
+++ main/coleta_acoes.py
@@ -4,28 +4,26 @@ import time
 import os
 
 
-path_chromedriver = os.getcwd() + '/chromedriver'
-browser = webdriver.Chrome(path_chromedriver)
-url = 'https://www.google.com/search?q=petr4'
-browser.get(url)
-time.sleep(5)  # esperar 5 segundos para carregar os dados do site.
-html = browser.page_source
-browser.close()
-soup = BeautifulSoup(html, features='html.parser')
+def coleta_dados_acao(ticker_acao):
 
-valor_abertura = soup.find('div', {'class':'PZPZlf', 'data-attrid':'Abertura'}).get_text()
-capitalizacao = soup.find('div', {'class':'PZPZlf', 'data-attrid':'Cap. merc.'}).get_text()
-dividendos = soup.find('div', {'class':'PZPZlf', 'data-attrid':'Rend. div.'}).get_text()
-indice_pl = soup.find('div', {'class':'PZPZlf', 'data-attrid':'Índice P/L'}).get_text()
-fechamento = soup.find('span', {'class':'IsqQVc NprOob wT3VGc', 'jsname':'vWLAgc'}).get_text()
+    path_chromedriver = os.getcwd() + '/chromedriver'
+    browser = webdriver.Chrome(path_chromedriver)
+    url = f'https://www.google.com/search?q={ticker_acao}'
+    browser.get(url)
+    time.sleep(5)  # esperar 5 segundos para carregar os dados do site.
+    html = browser.page_source
+    browser.close()
+    soup = BeautifulSoup(html, features='html.parser')
 
-valor_abertura = float(valor_abertura.replace(',', '.'))
-indice_pl = float(indice_pl.replace(',', '.'))
-fechamento = float(fechamento.replace(',', '.'))
+    dados_acao = {}
+    valor_abertura = soup.find('div', {'class':'PZPZlf', 'data-attrid':'Abertura'}).get_text()
+    dados_acao['capitalizacao'] = soup.find('div', {'class':'PZPZlf', 'data-attrid':'Cap. merc.'}).get_text()
+    dados_acao['dividendos'] = soup.find('div', {'class':'PZPZlf', 'data-attrid':'Rend. div.'}).get_text()
+    indice_pl = soup.find('div', {'class':'PZPZlf', 'data-attrid':'Índice P/L'}).get_text()
+    fechamento = soup.find('span', {'class':'IsqQVc NprOob wT3VGc', 'jsname':'vWLAgc'}).get_text()
 
+    dados_acao['valor_abertura'] = float(valor_abertura.replace(',', '.'))
+    dados_acao['indice_pl'] = float(indice_pl.replace(',', '.'))
+    dados_acao['fechamento'] = float(fechamento.replace(',', '.'))
 
-print(valor_abertura)
-print(type(valor_abertura), type(capitalizacao))
-print('dividendos', dividendos, type(dividendos))
-print('indice', indice_pl, type(indice_pl))
-print('fechamento', fechamento, type(fechamento))
+    return dados_acao
--- main/coleta_empresas.py
+++ main/coleta_empresas.py
@@ -5,48 +5,53 @@ import requests
 import os
 
 
-path_chromedriver = os.getcwd() + '/chromedriver'
-browser = webdriver.Chrome(path_chromedriver)
-url = 'https://www.google.com/finance/quote/PETR4:BVMF?sa=X&ved=2ahUKEwi6' \
-    '5caWqvn3AhUtkpUCHeC7C-MQ3ecFegQIAxAi'
+def coleta_dados_empresa(ticker_acao):
 
-browser.get(url)
-time.sleep(5)  # esperar 5 segundos para carregar os dados do site.
-html = browser.page_source
-browser.close()
-soup_browser = BeautifulSoup(html, features='html.parser')
-ceo = soup_browser.find(
-    'a',
-    {
-        'class': 'tBHE4e',
-        'target': '_blank',
-        'href': 'https://www.google.com/search?q=Jos%C3%A9%20Mauro%20Ferreira%20Coelho&hl=pt-BR'
-    }
-).get_text()
+    path_chromedriver = os.getcwd() + '/chromedriver'
+    browser = webdriver.Chrome(path_chromedriver)
+    url = f'https://www.google.com/search?q={ticker_acao}'
+    browser.get(url)
+    time.sleep(5)  # esperar 5 segundos para carregar os dados do site.
+    html = browser.page_source
+    browser.close()
+    soup = BeautifulSoup(html, features='html.parser')
+    url_dados_empresa = soup.find('a', {'class':'tiS4rf Q2MMlc'})['href']
+    
+    browser = webdriver.Chrome(path_chromedriver)
+    browser.get(url_dados_empresa)
+    time.sleep(5)  # esperar 5 segundos para carregar os dados do site.
+    html = browser.page_source
+    browser.close()
+    soup_browser = BeautifulSoup(html, features='html.parser')
+    dados_empresa = {}
+    dados_empresa['ceo'] = soup_browser.find(
+        'a',
+        {
+            'class': 'tBHE4e',
+            'target': '_blank',
+            'href': 'https://www.google.com/search?q=Jos%C3%A9%20Mauro%20Ferreira%20Coelho&hl=pt-BR'
+        }
+    ).get_text()
 
-html = requests.get(url)
-soup = BeautifulSoup(html.content, 'html.parser')
-acao_nome = soup.find('div', {'role':'heading', 'class':'zzDege', }).get_text()
-data_fundacao = soup.findAll('div', {'class':'P6K39c'})[9].get_text()
-sede = soup.findAll(
-    'a',
-    {
-        'target': '_blank',
-        'class': 'tBHE4e',
-    }
-)[1].get_text()
+    dados_empresa['acao_nome'] = soup_browser.find('div', {'role':'heading', 'class':'zzDege', }).get_text()
+    dados_empresa['data_fundacao'] = soup_browser.findAll('div', {'class':'P6K39c'})[10].get_text()
+    dados_empresa['sede'] = soup_browser.find(
+        'a',
+        {
+            'target': '_blank',
+            'class': 'tBHE4e',
+            'href': 'https://www.google.com/maps/place/Av.%20Republica%20do%20Chile%2C%20Rio%20de%20Janeiro%2C%20Rio'\
+                '%20de%20Janeiro%2C%20Brasil?hl=pt-BR'
+        }
+    ).get_text()
 
-site = soup.findAll(
-    'a',
-    {
-        'target': '_blank',
-        'class': 'tBHE4e',
-    }
-)[2].get_text()
+    dados_empresa['site'] = soup_browser.find(
+        'a',
+        {
+            'target': '_blank',
+            'class': 'tBHE4e',
+            'href': 'http://petrobras.com.br/'
+        }
+    ).get_text()
 
-
-print(acao_nome)
-print(ceo)
-print(data_fundacao)
-print(sede)
-print(site)
+    return dados_empresa
